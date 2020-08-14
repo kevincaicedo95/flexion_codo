957ea73fa97bcc0d2bb2d4a8447986da399cb421
kevincaicedo95 <65618665+kevincaicedo95@users.noreply.github.com>
2020-08-14 17:00:14 -0500

    Add files via upload

--- codigo flexion.py
+++ codigo flexion.py
@@ -4,26 +4,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def rotate_bound(image, angle): ##funcion para rotar la imagen
-    # grab the dimensions of the image and then determine the
-    # center
-    (h, w) = image.shape[:2]
-    (cX, cY) = (w // 2, h // 2)
-    # grab the rotation matrix (applying the negative of the
-    # angle to rotate clockwise), then grab the sine and cosine
-    # (i.e., the rotation components of the matrix)
-    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1])
-    # compute the new bounding dimensions of the image
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin))
-    # adjust the rotation matrix to take into account translation
-    M[0, 2] += (nW / 2) - cX
-    M[1, 2] += (nH / 2) - cY
-    # perform the actual rotation and return the image
-    return cv2.warpAffine(image, M, (nW, nH))
-
 def circulos(img):
     #Encuentra los circulos presentes en la imagen 
     circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,8,param1=50,param2=10,minRadius=10,maxRadius=18)
